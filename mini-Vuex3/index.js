93041dbcc399bed6d5edda1fff0445876eb67e6e
ZJQX1607 <1791254914@qq.com>
2024-07-23 09:01:09 +0800

    单个vuex模块基本实现

--- mini-Vuex3/index.js
+++ mini-Vuex3/index.js
@@ -6,10 +6,55 @@ class Store {
         let getters = options.getters
         let mutations = options.mutations
         let actions = options.actions
-        // let modules = options.modules
 
-        this.state = state
-        
+        this.getters = {}
+        this.mutations = options.mutations
+        this.actions = options.actions
+
+        const computed = {}
+
+        Object.keys(getters).forEach(key => {
+
+            computed[key] = () => {
+                return getters[key](this.state)
+            }
+
+            // 代理，取 getter 时返回 computed 的值
+            Object.defineProperty(this.getters, key, {
+                get: () => {
+                    console.log('this', this);
+                    return this._vm[key]
+                }
+            })
+        })
+
+        this._vm = new Vue({
+            data: {
+                // 将 state 存放在 vue 的 data 中，目的是为其添加响应式
+                // 添加 $ 是为了不让 vue 将 state 直接代理到 vm 上，不希望用户可以直接这样访问到 state
+                $$state: state
+            },
+            // 用于实现计算属性
+            computed,
+        })
+
+
+
+    }
+
+    // 取 state 时需要向 vm 上去取
+    get state() {
+        return this._vm._data.$$state
+    }
+
+    // 通过 commit 调用 mutations
+    commit = (funcName, payload) => {
+        this.mutations[funcName](this.state, payload)
+    }
+
+    // 通过 dispatch 调用 actions
+    dispatch = (funcName, payload) => {
+        this.actions[funcName](this, payload)
     }
 }
 
